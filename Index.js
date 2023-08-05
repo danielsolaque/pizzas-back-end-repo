0d8a1d30eb0876e7d5f5e8c178d188050b19d6e5
Daniel Solaque <danielsolaque@pursuit.org>
2023-08-05 18:21:53 -0400

    adding back end code

---
+++ Index.js
@@ -0,0 +1,56 @@
+//Importing all dotenv package to the const dotenv.
+const dotenv = require('dotenv')
+
+//load the env to the server
+dotenv.config()
+
+// Importing all express package to the const express.
+const express = require('express');
+
+//"file system" allow us interact with the file system in this case is (pizza.json)
+const fs = require('fs');
+
+//Creating my aplication
+const app = express();
+
+
+const PORT = process.env.PORT || 8888;
+
+// Allow to my server read data from json. 
+app.use(express.json());
+
+
+//Protocolo http first Verb.
+
+// Health check route
+//when request
+app.get('/', (req, res) => { //==
+  res.json({ data: 'Service is running!' });
+});
+
+
+
+//Request and responses are objects that are generated once the client access to the route pre-define.
+app.get('/items', (req, res) => {
+    const pizzas = fs.readFileSync('pizzas.json')
+    const parsePizzas = JSON.parse(pizzas)
+    res.json({ data: parsePizzas });
+})
+
+app.get('/items/:id', (req, res) => {
+    const id = req.params.id //params allow us to get the values sent by the user through the Url + params is a property of the request object.
+
+    const pizzas = fs.readFileSync('pizzas.json')
+    const parsePizzas = JSON.parse(pizzas)
+
+    const found = parsePizzas.find((pizza) => pizza.id === id)
+    if (!found) {
+        res.status(404).json({ error: 'Id not found' })
+    } else {
+        res.json({ data: found }) ///by default express response with a 200 status code.
+    }
+})
+
+
+app.listen(PORT,() => console.log ('listening') )
+
