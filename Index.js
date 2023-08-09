c23b7c0aa9ab563947f8ed58b321e534e284e42d
Daniel Solaque <danielsolaque@pursuit.org>
2023-08-08 22:54:01 -0400

    fixing problems provided by the instructor in the feedback

--- Index.js
+++ Index.js
@@ -1,16 +1,15 @@
 //Importing all dotenv package to the const dotenv.
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 
 //load the env to the server
-dotenv.config()
+dotenv.config();
 
 // Importing all express package to the const express.
-const express = require('express');
-
-const cors = require('cors'); // Import the cors package
+const express = require("express");
+const cors = require("cors"); // Import the cors package
 
 //"file system" allow us interact with the file system in this case is (pizza.json)
-const fs = require('fs');
+const pizzas = require("./pizzas.json");
 
 //Creating my aplication
 const app = express();
@@ -20,49 +19,38 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8888;
 
-// Allow to my server read data from json. 
+// Allow to my server read data from json.
 app.use(express.json());
-
 
 //Protocolo http first Verb.
 
 // Health check route
 //when request
-app.get('/', (req, res) => { //==
-  res.json({ data: 'Service is running!' });
+app.get("/", (req, res) => {
+  res.json({ data: "Service is running!" });
 });
 
-
-
 //Request and responses are objects that are generated once the client access to the route pre-define.
-app.get('/items', (req, res) => {
-    const pizzas = fs.readFileSync('pizzas.json')
-    const parsePizzas = JSON.parse(pizzas)
-    res.json({ data: parsePizzas });
-})
+app.get("/items", (req, res) => {
+  res.json({ data: pizzas });
+});
 
-
-//Mike you can test the error at the front end uncommenting this code and commenting the above: 
+//Mike you can test the error at the front end uncommenting this code and commenting the above:
 
 // app.get("/items", (req, res) => {
 //   res.status(500).json({ error: "Server Error" });
 // });
 
+app.get("/items/:id", (req, res) => {
+  const id = req.params.id; //params allow us to get the values sent by the user through the Url + params is a property of the request object.
 
-app.get('/items/:id', (req, res) => {
-    const id = req.params.id //params allow us to get the values sent by the user through the Url + params is a property of the request object.
+  const found = pizzas.find((pizza) => pizza.id === id);
 
-    const pizzas = fs.readFileSync('pizzas.json')
-    const parsePizzas = JSON.parse(pizzas)
+  if (!found) {
+    res.status(404).json({ error: "Id not found" });
+  } else {
+    res.json({ data: found }); /// by default express response with a 200 status code.
+  }
+});
 
-    const found = parsePizzas.find((pizza) => pizza.id === id)
-    if (!found) {
-        res.status(404).json({ error: 'Id not found' })
-    } else {
-        res.json({ data: found }) ///by default express response with a 200 status code.
-    }
-})
-
-
-app.listen(PORT,() => console.log ('listening') )
-
+app.listen(PORT, () => console.log(`Server running and listening on port ${PORT}`));
