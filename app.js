55e5530ac3e277c4a1fed8fd9c152ab3d63cbcec
Daniel Solaque <danielsolaque@pursuit.org>
2023-08-13 15:11:43 -0400

    adding try / catch to the controller.

--- app.js
+++ app.js
@@ -6,27 +6,36 @@ const pizzas = require("./pizzas.json");
 const app = express();
 
 app.use(cors());
-
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ data: "Service is running!" });
+  try {
+    res.json({ data: "Service is running!" });
+  } catch (error) {
+    res.status(500).json({ error: "An internal server error occurred" });
+  }
 });
 
 app.get("/items", (req, res) => {
-  res.json({ data: pizzas });
+  try {
+    res.json({ data: pizzas });
+  } catch (error) {
+    res.status(500).json({ error: "An internal server error occurred" });
+  }
 });
 
 app.get("/items/:id", (req, res) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
+    const found = pizzas.find((pizza) => pizza.id === id);
 
-  const found = pizzas.find((pizza) => pizza.id === id);
-
-  if (!found) {
-    res.status(404).json({ error: "Id not found" });
-  } else {
-    res.json({ data: found });
+    if (!found) {
+      res.status(404).json({ error: "Id not found" });
+    } else {
+      res.json({ data: found });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "An internal server error occurred" });
   }
 });
 
